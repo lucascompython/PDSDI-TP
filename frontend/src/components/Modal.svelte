2eb6718a6f9a5a0ed4b6ceba50f92f0abd8e51fe
BijouTOP <guilhermeroquesousa@hotmail.com>
2024-11-26 15:25:55 +0000

    Menino quer brigar

---
+++ frontend/src/components/Modal.svelte
@@ -0,0 +1,81 @@
+<script>
+  let { header, children } = $props();
+
+  let dialog = $state(); // HTMLDialogElement
+
+  $effect(() => {
+    if (showModal) dialog.showModal();
+  });
+
+  let showModal = $state(false);
+</script>
+
+<button onclick={() => (showModal = true)}>
+  <img src="/plus_square.svg" alt="Add" />
+</button>
+
+<!-- svelte-ignore a11y_click_events_have_key_events, a11y_no_noninteractive_element_interactions -->
+<dialog
+  bind:this={dialog}
+  onclose={() => (showModal = false)}
+  onclick={(e) => {
+    if (e.target === dialog) dialog.close();
+  }}
+>
+  <div>
+    {@render header?.()}
+    <hr />
+    {@render children?.()}
+    <hr />
+    <!-- svelte-ignore a11y_autofocus -->
+    <button autofocus onclick={() => dialog.close()}>close modal</button>
+  </div>
+</dialog>
+
+<style>
+  dialog {
+    max-width: 32em;
+    border-radius: 0.2em;
+    border: none;
+    padding: 0;
+    background-color: black;
+  }
+  dialog::backdrop {
+    background: black;
+  }
+  dialog > div {
+    padding: 1em;
+  }
+  dialog[open] {
+    animation: zoom 0.3s cubic-bezier(0.34, 1.56, 0.64, 1);
+  }
+  @keyframes zoom {
+    from {
+      transform: scale(0.95);
+    }
+    to {
+      transform: scale(1);
+    }
+  }
+  dialog[open]::backdrop {
+    animation: fade 0.2s ease-out;
+  }
+  @keyframes fade {
+    from {
+      opacity: 0;
+    }
+    to {
+      opacity: 1;
+    }
+  }
+  button {
+    display: block;
+    margin: auto;
+    width: 200px;
+    height: 200px;
+    background: none;
+    border: none;
+    padding: 0;
+    cursor: pointer;
+  }
+</style>
