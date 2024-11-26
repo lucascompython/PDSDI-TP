43480b95c97dc0285c31d3fa9b6675de0f582f0e
Lucas de Linhares <lucasdelinhares@gmail.com>
2024-11-26 15:26:34 +0000

    Add backend deps

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -3,11 +3,24 @@ use mimalloc::MiMalloc;
 #[global_allocator]
 static GLOBAL: MiMalloc = MiMalloc;
 
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use serde::Serialize;
+
+#[derive(Serialize)]
+struct MyObj {
+    name: String,
+    age: u8,
+}
 
 #[get("/")]
 async fn index() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+    let data = MyObj {
+        name: "Rust".to_string(),
+        age: 8,
+    };
+
+    // HttpResponse::Ok().json(data)
+    web::Json(data)
 }
 
 #[actix_web::main]
