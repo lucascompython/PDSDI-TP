071a04043dbd0ecc7647e1539e0b40910abe8ab7
BijouTOP <guilhermeroquesousa@hotmail.com>
2024-12-26 18:49:12 +0000

    MMOO DDAALL

--- frontend/src/components/Modal.svelte
+++ frontend/src/components/Modal.svelte
@@ -1,68 +1,28 @@
 <script>
-  let { showModal = $bindable(), header, children } = $props();
-
-  let dialog = $state(); // HTMLDialogElement
-
-  $effect(() => {
-    if (showModal) dialog.showModal();
-  });
+  import AddButtonIcon from "./AddButtonIcon.svelte";
 </script>
 
-<!-- svelte-ignore a11y_click_events_have_key_events, a11y_no_noninteractive_element_interactions -->
-<dialog
-  bind:this={dialog}
-  onclose={() => (showModal = false)}
-  onclick={(e) => {
-    if (e.target === dialog) dialog.close();
-  }}
->
-  <div>
-    {@render header?.()}
-    <hr />
-    {@render children?.()}
-    <hr />
-    <!-- svelte-ignore a11y_autofocus -->
-    <button autofocus onclick={() => dialog.close()}>close modal</button>
+<!-- When u open the button moves? sus -->
+
+<button on:click={() => my_modal_2.showModal()}><AddButtonIcon /></button>
+<dialog id="my_modal_2" class="modal">
+  <div class="modal-box">
+    <h3 class="text-lg font-bold">Hello!</h3>
+    <p class="py-4">Press ESC key or click outside to close</p>
   </div>
+  <form method="dialog" class="modal-backdrop">
+    <button>close</button>
+  </form>
 </dialog>
 
 <style>
-  dialog {
-    max-width: 32em;
-    border-radius: 0.2em;
-    border: none;
-    padding: 0;
-    background-color: var(--bg-color);
-  }
-  dialog::backdrop {
-    background: var(--bg-color);
-  }
-  dialog > div {
-    padding: 1em;
-  }
-  dialog[open] {
-    animation: zoom 0.3s cubic-bezier(0.34, 1.56, 0.64, 1);
-  }
-  @keyframes zoom {
-    from {
-      transform: scale(0.95);
-    }
-    to {
-      transform: scale(1);
-    }
-  }
-  dialog[open]::backdrop {
-    animation: fade 0.2s ease-out;
-  }
-  @keyframes fade {
-    from {
-      opacity: 0;
-    }
-    to {
-      opacity: 1;
-    }
-  }
   button {
-    display: block;
+    background-color: none;
+    border: none;
+    border-radius: none;
+  }
+
+  button:hover {
+    background-color: none;
   }
 </style>
