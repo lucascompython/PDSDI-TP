388475a9f35aac20864c0bedfcaaff01c9e615a9
BijouTOP <guilhermeroquesousa@hotmail.com>
2024-12-28 00:31:01 +0000

    CAT~~MEOW

--- frontend/src/components/DropdownColor.svelte
+++ frontend/src/components/DropdownColor.svelte
@@ -6,29 +6,153 @@
   const t = useTranslations(lang);
 </script>
 
-<label class="form-control w-full max-w-xs">
-  <div class="label">
-    <span class="label-text">{t("colorO.dropdownLabel")}</span>
-  </div>
-  <select class="select select-bordered">
-    <option disabled selected hidden>{t("dropdown.firstOption")}</option>
-    <option>-</option>
-    <option>-</option>
-    <option>-</option>
-    <option>-</option>
-    <option>-</option>
-  </select>
-</label>
+<details class="dropdown">
+  <summary class="btn m-1">{t("colorO.dropdownLabel")}</summary>
+  <ul
+    class="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow"
+  >
+    <li>
+      <div class="form-control">
+        <label class="label cursor-pointer">
+          <span class="label-text mr-1">{t("colorO.red")}-</span>
+          <input
+            type="checkbox"
+            checked={true}
+            class="checkbox [--chkbg:#FF0000] [--chkfg:black]"
+          />
+        </label>
+      </div>
+    </li>
+    <li>
+      <div class="form-control">
+        <label class="label cursor-pointer">
+          <span class="label-text mr-1">{t("colorO.white")}-</span>
+          <input
+            type="checkbox"
+            checked={true}
+            class="checkbox [--chkbg:#FFFFFF] [--chkfg:black]"
+          />
+        </label>
+      </div>
+    </li>
+    <li>
+      <div class="form-control">
+        <label class="label cursor-pointer">
+          <span class="label-text mr-1">{t("colorO.blue")}-</span>
+          <input
+            type="checkbox"
+            checked={true}
+            class="checkbox [--chkbg:#0000FF] [--chkfg:black]"
+          />
+        </label>
+      </div>
+    </li>
+    <li>
+      <div class="form-control">
+        <label class="label cursor-pointer">
+          <span class="label-text mr-1">{t("colorO.black")}-</span>
+          <input
+            type="checkbox"
+            checked={true}
+            class="checkbox [--chkbg:black] [--chkfg:white]"
+          />
+        </label>
+      </div>
+    </li>
+    <li>
+      <div class="form-control">
+        <label class="label cursor-pointer">
+          <span class="label-text mr-1">{t("colorO.brown")}-</span>
+          <input
+            type="checkbox"
+            checked={true}
+            class="checkbox [--chkbg:#8B4513] [--chkfg:black]"
+          />
+        </label>
+      </div>
+    </li>
+    <li>
+      <div class="form-control">
+        <label class="label cursor-pointer">
+          <span class="label-text mr-1">{t("colorO.pink")}-</span>
+          <input
+            type="checkbox"
+            checked={true}
+            class="checkbox [--chkbg:#FFC0CB] [--chkfg:black]"
+          />
+        </label>
+      </div>
+    </li>
+    <li>
+      <div class="form-control">
+        <label class="label cursor-pointer">
+          <span class="label-text mr-1">{t("colorO.purple")}-</span>
+          <input
+            type="checkbox"
+            checked={true}
+            class="checkbox [--chkbg:#800080] [--chkfg:black]"
+          />
+        </label>
+      </div>
+    </li>
+    <li>
+      <div class="form-control">
+        <label class="label cursor-pointer">
+          <span class="label-text mr-1">{t("colorO.yellow")}-</span>
+          <input
+            type="checkbox"
+            checked={true}
+            class="checkbox [--chkbg:#FFFF00] [--chkfg:black]"
+          />
+        </label>
+      </div>
+    </li>
+    <li>
+      <div class="form-control">
+        <label class="label cursor-pointer">
+          <span class="label-text mr-1">{t("colorO.orange")}-</span>
+          <input
+            type="checkbox"
+            checked={true}
+            class="checkbox [--chkbg:#FFA500] [--chkfg:black]"
+          />
+        </label>
+      </div>
+    </li>
+    <li>
+      <div class="form-control">
+        <label class="label cursor-pointer">
+          <span class="label-text mr-1">{t("colorO.grey")}-</span>
+          <input
+            type="checkbox"
+            checked={true}
+            class="checkbox [--chkbg:#808080] [--chkfg:black]"
+          />
+        </label>
+      </div>
+    </li>
+  </ul>
+</details>
 
 <style>
-  option {
+  ul {
+    background-color: none;
+  }
+  :global(.dark) summary {
+    background-color: #0d1117;
+  }
+  :global(.dark) summary:hover {
+    background-color: #0d1117;
+  }
+  summary {
+    background-color: white;
+    border: 1px solid gray;
     color: var(--text-color);
+  }
+  summary:hover {
+    background-color: white;
   }
   span {
-    color: var(--text-color);
-  }
-  select {
-    background-color: var(--bg-color);
     color: var(--text-color);
   }
 </style>
