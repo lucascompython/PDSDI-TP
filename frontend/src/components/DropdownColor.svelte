508d622fe7ddf411b14a8470b08e3b9d0078c67d
BijouTOP <guilhermeroquesousa@hotmail.com>
2025-01-06 01:17:43 +0000

    imposter among us " page "

--- frontend/src/components/DropdownColor.svelte
+++ frontend/src/components/DropdownColor.svelte
@@ -6,153 +6,269 @@
   const t = useTranslations(lang);
 </script>
 
-<details class="dropdown">
-  <summary class="btn m-1">{t("colorO.dropdownLabel")}</summary>
-  <ul
-    class="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow"
+<span class="text-lg">{t("colorO.label")}</span>
+
+{#if window.innerWidth < 600}
+  <div>
+    <div>
+      <label class="fieldset-label">
+        <input
+          type="checkbox"
+          checked={false}
+          class="checkbox [--chkbg:#FF0000] [--chkfg:black]"
+        />
+        {t("colorO.red")}
+      </label>
+      <label class="fieldset-label">
+        <input
+          type="checkbox"
+          checked={false}
+          class="checkbox [--chkbg:#FFFFFF] [--chkfg:black]"
+        />
+        {t("colorO.white")}
+      </label>
+      <label class="fieldset-label">
+        <input
+          type="checkbox"
+          checked={false}
+          class="checkbox [--chkbg:#000000] [--chkfg:white]"
+        />
+        {t("colorO.black")}
+      </label>
+    </div>
+    <div>
+      <label class="fieldset-label">
+        <input
+          type="checkbox"
+          checked={false}
+          class="checkbox [--chkbg:#FFC0CB] [--chkfg:black]"
+        />
+        {t("colorO.pink")}
+      </label>
+
+      <label class="fieldset-label">
+        <input
+          type="checkbox"
+          checked={false}
+          class="checkbox [--chkbg:#800080] [--chkfg:black]"
+        />
+        {t("colorO.purple")}
+      </label>
+
+      <label class="fieldset-label">
+        <input
+          type="checkbox"
+          checked={false}
+          class="checkbox [--chkbg:#FFFF00] [--chkfg:black]"
+        />
+        {t("colorO.yellow")}
+      </label>
+    </div>
+    <div>
+      <label class="fieldset-label">
+        <input
+          type="checkbox"
+          checked={false}
+          class="checkbox [--chkbg:#FFA500] [--chkfg:black]"
+        />
+        {t("colorO.orange")}
+      </label>
+      <label class="fieldset-label">
+        <input
+          type="checkbox"
+          checked={false}
+          class="checkbox [--chkbg:#808080] [--chkfg:black]"
+        />
+        {t("colorO.grey")}
+      </label>
+
+      <label class="fieldset-label">
+        <input
+          type="checkbox"
+          checked={false}
+          class="checkbox [--chkbg:#008000] [--chkfg:black]"
+        />
+        {t("colorO.green")}
+      </label>
+    </div>
+    <div>
+      <label class="fieldset-label">
+        <input
+          type="checkbox"
+          checked={false}
+          class="checkbox [--chkbg:#0000FF] [--chkfg:black]"
+        />
+        {t("colorO.blue")}
+      </label>
+      <label class="fieldset-label">
+        <input
+          type="checkbox"
+          checked={false}
+          class="checkbox [--chkbg:#A52A2A] [--chkfg:black]"
+        />
+        {t("colorO.brown")}
+      </label>
+      <label class="fieldset-label">
+        <input
+          type="checkbox"
+          checked={false}
+          class="checkbox [--chkbg:#FFD700] [--chkfg:black]"
+        />
+        {t("colorO.gold")}
+      </label>
+    </div>
+  </div>
+{:else}
+  <div>
+    <div>
+      <label class="fieldset-label">
+        <input
+          type="checkbox"
+          checked={false}
+          class="checkbox [--chkbg:#FF0000] [--chkfg:black]"
+        />
+        {t("colorO.red")}
+      </label>
+      <label class="fieldset-label">
+        <input
+          type="checkbox"
+          checked={false}
+          class="checkbox [--chkbg:#FFFFFF] [--chkfg:black]"
+        />
+        {t("colorO.white")}
+      </label>
+      <label class="fieldset-label">
+        <input
+          type="checkbox"
+          checked={false}
+          class="checkbox [--chkbg:#000000] [--chkfg:white]"
+        />
+        {t("colorO.black")}
+      </label>
+      <label class="fieldset-label">
+        <input
+          type="checkbox"
+          checked={false}
+          class="checkbox [--chkbg:#FFC0CB] [--chkfg:black]"
+        />
+        {t("colorO.pink")}
+      </label>
+    </div>
+    <div>
+      <label class="fieldset-label">
+        <input
+          type="checkbox"
+          checked={false}
+          class="checkbox [--chkbg:#800080] [--chkfg:black]"
+        />
+        {t("colorO.purple")}
+      </label>
+
+      <label class="fieldset-label">
+        <input
+          type="checkbox"
+          checked={false}
+          class="checkbox [--chkbg:#FFFF00] [--chkfg:black]"
+        />
+        {t("colorO.yellow")}
+      </label>
+      <label class="fieldset-label">
+        <input
+          type="checkbox"
+          checked={false}
+          class="checkbox [--chkbg:#FFA500] [--chkfg:black]"
+        />
+        {t("colorO.orange")}
+      </label>
+      <label class="fieldset-label">
+        <input
+          type="checkbox"
+          checked={false}
+          class="checkbox [--chkbg:#808080] [--chkfg:black]"
+        />
+        {t("colorO.grey")}
+      </label>
+    </div>
+    <div>
+      <label class="fieldset-label">
+        <input
+          type="checkbox"
+          checked={false}
+          class="checkbox [--chkbg:#008000] [--chkfg:black]"
+        />
+        {t("colorO.green")}
+      </label>
+      <label class="fieldset-label">
+        <input
+          type="checkbox"
+          checked={false}
+          class="checkbox [--chkbg:#0000FF] [--chkfg:black]"
+        />
+        {t("colorO.blue")}
+      </label>
+      <label class="fieldset-label">
+        <input
+          type="checkbox"
+          checked={false}
+          class="checkbox [--chkbg:#A52A2A] [--chkfg:black]"
+        />
+        {t("colorO.brown")}
+      </label>
+      <label class="fieldset-label">
+        <input
+          type="checkbox"
+          checked={false}
+          class="checkbox [--chkbg:#FFD700] [--chkfg:black]"
+        />
+        {t("colorO.gold")}
+      </label>
+    </div>
+  </div>
+{/if}
+<div style="display: flex; justify-content: center; margin-top: 2rem;">
+  <button class="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg"
+    >{t("outfit.button")}</button
   >
-    <li>
-      <div class="form-control">
-        <label class="label cursor-pointer">
-          <span class="label-text mr-1">{t("colorO.red")}-</span>
-          <input
-            type="checkbox"
-            checked={true}
-            class="checkbox [--chkbg:#FF0000] [--chkfg:black]"
-          />
-        </label>
-      </div>
-    </li>
-    <li>
-      <div class="form-control">
-        <label class="label cursor-pointer">
-          <span class="label-text mr-1">{t("colorO.white")}-</span>
-          <input
-            type="checkbox"
-            checked={true}
-            class="checkbox [--chkbg:#FFFFFF] [--chkfg:black]"
-          />
-        </label>
-      </div>
-    </li>
-    <li>
-      <div class="form-control">
-        <label class="label cursor-pointer">
-          <span class="label-text mr-1">{t("colorO.blue")}-</span>
-          <input
-            type="checkbox"
-            checked={true}
-            class="checkbox [--chkbg:#0000FF] [--chkfg:black]"
-          />
-        </label>
-      </div>
-    </li>
-    <li>
-      <div class="form-control">
-        <label class="label cursor-pointer">
-          <span class="label-text mr-1">{t("colorO.black")}-</span>
-          <input
-            type="checkbox"
-            checked={true}
-            class="checkbox [--chkbg:black] [--chkfg:white]"
-          />
-        </label>
-      </div>
-    </li>
-    <li>
-      <div class="form-control">
-        <label class="label cursor-pointer">
-          <span class="label-text mr-1">{t("colorO.brown")}-</span>
-          <input
-            type="checkbox"
-            checked={true}
-            class="checkbox [--chkbg:#8B4513] [--chkfg:black]"
-          />
-        </label>
-      </div>
-    </li>
-    <li>
-      <div class="form-control">
-        <label class="label cursor-pointer">
-          <span class="label-text mr-1">{t("colorO.pink")}-</span>
-          <input
-            type="checkbox"
-            checked={true}
-            class="checkbox [--chkbg:#FFC0CB] [--chkfg:black]"
-          />
-        </label>
-      </div>
-    </li>
-    <li>
-      <div class="form-control">
-        <label class="label cursor-pointer">
-          <span class="label-text mr-1">{t("colorO.purple")}-</span>
-          <input
-            type="checkbox"
-            checked={true}
-            class="checkbox [--chkbg:#800080] [--chkfg:black]"
-          />
-        </label>
-      </div>
-    </li>
-    <li>
-      <div class="form-control">
-        <label class="label cursor-pointer">
-          <span class="label-text mr-1">{t("colorO.yellow")}-</span>
-          <input
-            type="checkbox"
-            checked={true}
-            class="checkbox [--chkbg:#FFFF00] [--chkfg:black]"
-          />
-        </label>
-      </div>
-    </li>
-    <li>
-      <div class="form-control">
-        <label class="label cursor-pointer">
-          <span class="label-text mr-1">{t("colorO.orange")}-</span>
-          <input
-            type="checkbox"
-            checked={true}
-            class="checkbox [--chkbg:#FFA500] [--chkfg:black]"
-          />
-        </label>
-      </div>
-    </li>
-    <li>
-      <div class="form-control">
-        <label class="label cursor-pointer">
-          <span class="label-text mr-1">{t("colorO.grey")}-</span>
-          <input
-            type="checkbox"
-            checked={true}
-            class="checkbox [--chkbg:#808080] [--chkfg:black]"
-          />
-        </label>
-      </div>
-    </li>
-  </ul>
-</details>
+</div>
 
 <style>
-  ul {
-    background-color: none;
-  }
-  :global(.dark) summary {
-    background-color: #0d1117;
-  }
-  :global(.dark) summary:hover {
-    background-color: #0d1117;
-  }
-  summary {
-    background-color: white;
-    border: 1px solid gray;
-    color: var(--text-color);
-  }
-  summary:hover {
-    background-color: white;
-  }
   span {
     color: var(--text-color);
+  }
+  div {
+    display: flex;
+    flex-direction: column;
+  }
+  div div {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    max-width: 97%;
+    margin-top: 2rem;
+  }
+  label {
+    min-width: 110px;
+    color: var(--text-color);
+  }
+  button {
+    margin-top: 2rem;
+    background-color: var(--text-color);
+    color: var(--bg-color);
+  }
+  button:hover {
+    background-color: var(--text-color);
+    color: var(--bg-color);
+  }
+  input {
+    border-color: var(--text-color);
+  }
+  @media only screen and (max-width: 600px) {
+    div div {
+      margin-top: 0.1rem;
+    }
+    label {
+      min-width: 80px;
+      font-size: 0.5rem;
+    }
   }
 </style>
---
+++ frontend/src/components/OutfitComponent(page).svelte
@@ -0,0 +1,93 @@
+<script lang="ts">
+  import { getLangFromUrl, useTranslations } from "../i18n/utils";
+
+  import DropdownTemperature from "./DropdownTemperature.svelte";
+  import DropdownColor from "./DropdownColor.svelte";
+
+  let { windowLocation }: { windowLocation: URL } = $props();
+  const lang = getLangFromUrl(windowLocation);
+  const t = useTranslations(lang);
+</script>
+
+{#if window.innerWidth < 600}
+  <div class="split-screen">
+    <div class="left">
+      <DropdownTemperature {windowLocation} />
+      <div class="margin-top">
+        <DropdownColor {windowLocation} />
+      </div>
+    </div>
+  </div>
+
+  <style>
+    main {
+      color: white;
+      display: flex;
+      flex-direction: column;
+      flex: 1;
+      background-color: #d9d9d9;
+    }
+    .split-screen {
+      flex: 1;
+      display: flex;
+      width: 100%;
+
+      justify-content: center;
+    }
+    :global(.dark) main {
+      background-color: #0d1117;
+    }
+    .margin-top {
+      margin-top: 1rem;
+    }
+  </style>
+{:else}
+  <div class="split-screen">
+    <div class="left">
+      <div class="margin-left">
+        <DropdownTemperature {windowLocation} />
+        <div class="margin-top">
+          <DropdownColor {windowLocation} />
+        </div>
+      </div>
+      <div class="line-horizontal"></div>
+    </div>
+    <div class="right">Right content</div>
+    <div class="line-horizontal"></div>
+  </div>
+
+  <style>
+    main {
+      color: white;
+      display: flex;
+      flex-direction: column;
+      flex: 1;
+      background-color: #d9d9d9;
+    }
+    .split-screen {
+      flex: 1;
+      display: flex;
+      width: 100%;
+      position: relative;
+    }
+    .left {
+      flex-basis: 50%;
+      display: flex;
+      flex-direction: column;
+    }
+    :global(.dark) main {
+      background-color: #0d1117;
+    }
+    .right {
+      border-left: 3px solid #b3b1b1;
+      flex-basis: 50%;
+    }
+    .margin-left {
+      margin-left: 2rem;
+    }
+
+    .margin-top {
+      margin-top: 1rem;
+    }
+  </style>
+{/if}
