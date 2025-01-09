ea5242eb1daefad221cb6b2ad836e18335ea957e
Lucas de Linhares <lucasdelinhares@gmail.com>
2025-01-09 09:42:00 +0000

    Remove daisyui themes

--- frontend/tailwind.config.mjs
+++ frontend/tailwind.config.mjs
@@ -1,17 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			// Remove custom background colors
-			backgroundColor: false,
-		},
-	},
-	
-	plugins: [
-		require('daisyui'),
-	],
-	corePlugins: {
-		backgroundColor: false, // Disable background color utilities
-	},
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      // Remove custom background colors
+      backgroundColor: false,
+    },
+  },
+  daisyui: {
+    // Remove DaisyUI themes
+    themes: [],
+  },
+
+  plugins: [require("daisyui")],
+  corePlugins: {
+    backgroundColor: false, // Disable background color utilities
+  },
+};
