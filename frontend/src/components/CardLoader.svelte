d2e31307e63dcfcb81f374c8713ae9026af6e15c
Lucas de Linhares <lucasdelinhares@gmail.com>
2025-02-03 14:43:47 +0000

    Add loader to clothes page

--- frontend/src/components/CardLoader.svelte
+++ frontend/src/components/CardLoader.svelte
@@ -8,14 +8,38 @@
   const t = useTranslations(getLangFromUrl(windowLocation));
 
   let clothes: ClotheResponse[] = $state([]);
+  let loading = $state(true);
 
   onMount(() => {
     getClothes().then((clothesResponse) => {
+      loading = false;
       clothes = clothesResponse;
     });
   });
 </script>
 
+{#if loading}
+  <div class="loading-container">
+    <span class="loading loading-ring loading-xl"></span>
+  </div>
+{/if}
+
 {#each clothes as clothe}
   <EyeCard {clothe} {t} />
 {/each}
+
+<style>
+  .loading-container {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .loading {
+    height: 100px;
+    width: 100px;
+  }
+</style>
